20a504c6559e03482817f9d9bb25cb68b202c300
niall-nsurely <niall-nsurely@users.noreply.github.com>
2022-06-01 17:29:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="nSurely"
+original_name="motorpy"
+original_urlname="motorpy"
+original_description="Awesome motorpy created by nSurely"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv motorpy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
