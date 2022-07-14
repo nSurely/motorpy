34edf6de473b12c9d9dd0e4229a1bebeaf12ab4a
Niall Crowley <niall@nsurely.com>
2022-07-14 17:48:21 +0100

    comments

--- motorpy/__init__.py
+++ motorpy/__init__.py
@@ -1,3 +1,23 @@
+# core class for motorpy
 from motorpy.base import Motor
+
+# auth needed for API requests
 from motorpy.auth import Auth
+
+# for advanced search on some methods
 from motorpy.search import Search
+
+# data models used to interact with records in the API
+from motorpy.models import (
+    Driver,
+    Vehicle,
+    DriverVehicle,
+    VehicleType,
+    Policy,
+    BillingAccount,
+    BillingEvent,
+    Fleet,
+    FleetDriver,
+    FleetVehicle,
+    CommonRisk
+)
