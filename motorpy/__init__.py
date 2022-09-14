cf9e9f88f63316962ca33ee5a43b461034860f0b
Niall <nialltcrowley@gmail.com>
2022-09-14 12:02:19 +0100

    pdoc ignore

--- motorpy/__init__.py
+++ motorpy/__init__.py
@@ -1,3 +1,16 @@
+# docs config
+__pdoc__ = {
+    "tests": False,
+    "actions": False,
+    "api": False,
+    "util": False,
+    "models.billing.tests": False,
+    "models.drivers.tests": False,
+    "models.fleets.tests": False,
+    "models.policy.tests": False,
+    "models.policy.tests": False
+}
+
 # core class for motorpy
 from motorpy.base import Motor
 
