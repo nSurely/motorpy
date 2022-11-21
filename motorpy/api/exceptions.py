56902534185efff4e6c71a297d5764b50f0d8a37
Niall Crowley <niall@nsurely.com>
2022-11-21 15:59:36 +0000

    better custom exceptions

--- motorpy/api/exceptions.py
+++ motorpy/api/exceptions.py
@@ -1,8 +1,37 @@
 
 class APIError(Exception):
     """Base class for all API exceptions"""
-    pass
+
+    def __init__(self, message: str, status_code: int = 500) -> None:
+        super().__init__(message)
+
+        self.message = message
+        self.status_code = status_code
+
+    def __str__(self) -> str:
+        return f"{self.status_code}: {self.message}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.status_code}, {self.message})"
+
+    def server_error(self) -> bool:
+        return self.status_code >= 500
+
+    def client_error(self) -> bool:
+        return self.status_code >= 400 and self.status_code < 500
+
+    def not_found(self) -> bool:
+        return self.status_code == 404
+
+    def bad_request(self) -> bool:
+        return self.status_code == 400
+
+    def unauthorized(self) -> bool:
+        return self.status_code == 401
+
 
 class APIAuthError(APIError):
     """Exception raised when the API returns an authentication error"""
-    pass
+
+    def __init__(self, message: str, status_code: int = 401) -> None:
+        super().__init__(message, status_code)
