584805767efe8694b1446e27a8bb118729b8e9a7
Niall Crowley <niall@nsurely.com>
2022-11-29 17:11:42 +0000

    bumped version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Inaza'
 author = 'Inaza'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
